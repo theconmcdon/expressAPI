a4be36eda06cba75c64f9a983eef78f7f7e9b285
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-07-15 12:34:57 -0500

    advanced not done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const chirpsRouter = require('./chirps')
-const usersRouter = require('./users')
 let router = express.Router();
 
 router.use('/chirps', chirpsRouter);
-router.use('/users', usersRouter);
 
 module.exports = router;
